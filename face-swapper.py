7b473a5a043eef26a91f5924d0b4d931413e0d09
KomorowskiKuba <komorii2k@gmail.com>
2020-11-24 15:10:45 +0100

    Added faces triangulation

--- face-swapper.py
+++ face-swapper.py
@@ -47,7 +47,47 @@ while cap.isOpened():
 
         faces = cv2.bitwise_and(frame, frame, mask = mask)
 
-        cv2.imshow("Face swap", faces)
+        rectangle1 = cv2.boundingRect(hull1)
+        #(x1, y1, w1, h1) = rectangle1
+        #cv2.rectangle(faces, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0))
+
+        rectangle2 = cv2.boundingRect(hull2)
+        #(x2, y2, w2, h2) = rectangle2
+        #cv2.rectangle(faces, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0))
+
+        subdiv_1 = cv2.Subdiv2D(rectangle1)
+        subdiv_1.insert(face1_landmarks.tolist())
+        triangles_1 = subdiv_1.getTriangleList()
+        triangles_1 = np.array(triangles_1, dtype=np.int32)
+        #print(triangles_1)
+
+        subdiv_2 = cv2.Subdiv2D(rectangle2)
+        subdiv_2.insert(face2_landmarks.tolist())
+        triangles_2 = subdiv_2.getTriangleList()
+        triangles_2 = np.array(triangles_2, dtype=np.int32)
+
+        for t in triangles_1:
+            pt1 = (t[0], t[1])
+            pt2 = (t[2], t[3])
+            pt3 = (t[4], t[5])
+
+            cv2.line(frame, pt1, pt2, (0, 0, 255), 2)
+            cv2.line(frame, pt2, pt3, (0, 0, 255), 2)
+            cv2.line(frame, pt1, pt3, (0, 0, 255), 2)
+
+        for t in triangles_2:
+            pt1 = (t[0], t[1])
+            pt2 = (t[2], t[3])
+            pt3 = (t[4], t[5])
+
+            cv2.line(frame, pt1, pt2, (0, 0, 255), 2)
+            cv2.line(frame, pt2, pt3, (0, 0, 255), 2)
+            cv2.line(frame, pt1, pt3, (0, 0, 255), 2)
+        #print(triangles_2)
+
+
+
+        cv2.imshow("Face swap", frame)
 
     else:
         cv2.imshow("Face swap", frame)
